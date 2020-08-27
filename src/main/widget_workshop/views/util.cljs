256ad64d5c3cd6070f192c7da59f1cf9af476f01
Chris Wasser <cawasser@mac.com>
2020-08-27 17:41:37 -0400

    - MOAR COLORS!

--- src/main/widget_workshop/views/util.cljs
+++ src/main/widget_workshop/views/util.cljs
@@ -1,10 +1,55 @@
 (ns widget-workshop.views.util)
 
-(def nextId (atom 0))
+(defonce nextId (atom 0))
 
 (def colors
-  [["blue" "white"]
-   ["white" "black"]])
+  [["AliceBlue" "black"]
+   ["AntiqueWhite" "black"]
+   ["Aqua" "black"]
+   ["Aquamarine" "black"]
+   ["Azure" "black"]
+   ["Beige" "black"]
+   ["Bisque" "black"]
+   ["Black" "white"]
+   ["BlanchedAlmond" "black"]
+   ["Blue" "white"]
+   ["BlueViolet" "black"]
+   ["Brown" "white"]
+   ["BurlyWood" "black"]
+   ["CadetBlue" "white"]
+   ["Chartreuse" "white"]
+   ["Chocolate" "white"]
+   ["Coral" "white"]
+   ["CornflowerBlue" "white"]
+   ["Cornsilk" "white"]
+   ["Crimson" "white"]
+   ["Cyan" "white"]
+   ["DarkBlue" "white"]
+   ["DarkCyan" "white"]
+   ["DarkGoldenRod" "white"]
+   ["DarkGray" "white"]
+   ["DarkGrey" "white"]
+   ["DarkGreen" "white"]
+   ["DarkKhaki" "white"]
+   ["DarkMagenta" "white"]
+   ["DarkOliveGreen" "white"]
+   ["DarkOrange" "white"]
+   ["DarkOrchid" "white"]
+   ["DarkRed" "white"]
+   ["DarkSalmon" "white"]
+   ["DarkSeaGreen" "white"]
+   ["DarkSlateBlue" "white"]
+   ["DarkSlateGray" "white"]
+   ["DarkSlateGrey" "white"]
+   ["DarkTurquoise" "white"]
+   ["DarkViolet" "white"]
+   ["DeepPink" "white"]
+   ["DeepSkyBlue" "white"]
+   ["DimGray" "white"]
+   ["DimGrey" "white"]
+   ["DodgerBlue" "white"]
+   ["FireBrick" "white"]
+   ["FloralWhite" "white"]])
 
 (defn- newColor []
   (nth colors (rand-int (count colors))))
@@ -14,8 +59,8 @@
 
 (defn make-new-widget [w]
   (let [[title-color text-color] (newColor)]
-    {:id w
-     :name (nextName)
+    {:id          w
+     :name        (nextName)
      :title-color title-color
-     :text-color text-color}))
+     :text-color  text-color}))
 
